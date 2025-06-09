79485a69ca15e2f87b642ec4c26c449e38c036f1
malirazaansari <malirazaansari4@gmail.com>
2025-06-09 20:44:56 +0500

    contact foam chagnese

--- src/components/Home/Contact/Contact.tsx
+++ src/components/Home/Contact/Contact.tsx
@@ -1,24 +1,31 @@
-import React from 'react'
-import ContactForm from './ContactForm'
-import SectionHeading from '@/components/helper/SectionHeading'
-import ContactInfo from './ContactInfo'
+import React from "react";
+import ContactForm from "./ContactForm";
+import SectionHeading from "@/components/helper/SectionHeading";
+import ContactInfo from "./ContactInfo";
 
 const Contact = () => {
   return (
-    <div className='pt-16 pb-16 dark:bg-[#191D2B] bg-gray-200'>
-        <SectionHeading>Contact Me</SectionHeading>
-        <div className="grid grid-cols-1 xl:grid-cols-2 w-[90%] sm:w-[80%] mx-auto items-center gap-10">
-        <div data-aos ="fade-left" data-aos-anchor-placement ="top-center" 
-                     className="mt-20">
-            <ContactForm/>
+    <div className="pt-16 pb-16 dark:bg-[#191D2B] bg-gray-200">
+      <SectionHeading>Contact Me</SectionHeading>
+      <div className="grid grid-cols-1 xl:grid-cols-2 w-[90%] sm:w-[80%] mx-auto items-center gap-10">
+        <div
+          data-aos="fade-left"
+          data-aos-anchor-placement="top-center"
+          className="mt-20"
+        >
+          <ContactForm />
         </div>
-        <div data-aos ="fade-right" data-aos-anchor-placement ="top-center" 
-                    data-aos-delay= "100" className='xl:mx-auto'>
-            <ContactInfo/>
+        <div
+          data-aos="fade-right"
+          data-aos-anchor-placement="top-center"
+          data-aos-delay="100"
+          className="xl:mx-auto"
+        >
+          <ContactInfo />
         </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- src/components/Home/Contact/ContactForm.tsx
+++ src/components/Home/Contact/ContactForm.tsx
@@ -1,17 +1,17 @@
-'use client'
-import React, { useState } from 'react';
+"use client";
+import React, { useState } from "react";
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    phone: '',
-    subject: '',
-    message: '',
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    subject: "",
+    message: "",
   });
 
-  const [status, setStatus] = useState<string>(''); // To handle success or error messages
+  const [status, setStatus] = useState<string>("");
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -23,49 +23,56 @@ const ContactForm = () => {
     setFormData({ ...formData, [name]: value });
   };
 
-
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-  e.preventDefault();
-  setStatus(''); // Reset status before submission
+    e.preventDefault();
+    setStatus(""); // Reset status before submission
 
-  // Validate form data
-  if (!formData.firstName || !formData.lastName || !formData.email || !formData.subject || !formData.message) {
-    setStatus('All fields are required.');
-    return;
-  }
-
-  try {
-    console.log('Submitting form data:', formData); // Debugging
-    const response = await fetch('/api/send-email', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    });
-
-    if (response.ok) {
-      const result = await response.json();
-      console.log('API response:', result);
-      setStatus('Message sent successfully!');
-      setFormData({
-        firstName: '',
-        lastName: '',
-        email: '',
-        phone: '',
-        subject: '',
-        message: '',
-      });
-    } else {
-      const errorData = await response.json();
-      console.error('Error Response:', errorData);
-      setStatus(errorData.error || 'Failed to send your message. Please try again.');
+    // Validate form data
+    if (
+      !formData.firstName ||
+      !formData.lastName ||
+      !formData.email ||
+      !formData.subject ||
+      !formData.message
+    ) {
+      setStatus("All fields are required.");
+      return;
     }
-  } catch (error) {
-    console.error('Error:', error);
-    setStatus('An error occurred. Please try again later.');
-  }
-};
+
+    try {
+      console.log("Submitting form data:", formData);
+      const response = await fetch("/api/send-email", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        console.log("API response:", result);
+        setStatus("Message sent successfully!");
+        setFormData({
+          firstName: "",
+          lastName: "",
+          email: "",
+          phone: "",
+          subject: "",
+          message: "",
+        });
+      } else {
+        const errorData = await response.json();
+        console.error("Error Response:", errorData);
+        setStatus(
+          errorData.error || "Failed to send your message. Please try again."
+        );
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      setStatus("An error occurred. Please try again later.");
+    }
+  };
 
   return (
     <div className="dark:bg-[#262d42] bg-gray-400 rounded-lg p-4 sm:p-10">
@@ -73,10 +80,14 @@ const ContactForm = () => {
         Let&apos;s Work together!
       </h1>
       <p className="dark:text-gray-200 text-gray-700 mt-3 lg:text-base text-xs md:text-sm">
-        For any questions or inquiries about my Services, Please don&apos;t hesitate to reach out to me. I&apos;d be happy to assist you.
+        For any questions or inquiries about my Services, Please don&apos;t
+        hesitate to reach out to me. I&apos;d be happy to assist you.
       </p>
-      
-      <form onSubmit={handleSubmit} className="mt-8 block w-full overflow-hidden">
+
+      <form
+        onSubmit={handleSubmit}
+        className="mt-8 block w-full overflow-hidden"
+      >
         <div className="flex flex-col md:flex-row items-center justify-center gap-4">
           <input
             type="text"
@@ -131,14 +142,15 @@ const ContactForm = () => {
           rows={5}
           className="flex-1 mt-5 dark:bg-[#454E56] bg-gray-300 dark:placeholder:text-white placeholder:text-gray-700 px-6 py-3 rounded-md border-[1.5px] border-gray-200 border-opacity-15 outline-none w-full"
         ></textarea>
-        {status && <p className="mt-4 text-center text-lg text-green-600">{status}</p>}
+        {status && (
+          <p className="mt-4 text-center text-lg text-green-600">{status}</p>
+        )}
         <div className="mt-5">
-            
           <button
             type="submit"
             className="px-6 py-3.5 dark:bg-[#191D2B] bg-blue-700 border-[#4A62D6] text-white hover:bg-[#4A62D6] transition-all duration-150 rounded-full"
           >
-             Submit Message
+            Submit Message
           </button>
         </div>
       </form>
